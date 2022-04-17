c98a5c233a7096c4b00206f83e837ebe60ee0109
juangh15 <47037988+juangh15@users.noreply.github.com>
2022-04-17 17:02:14 -0500

    lint installed and working google test example

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    "cypress"
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-force": "warn"
+  },
+  env: {
+    "cypress/globals": true
+  }
+};
